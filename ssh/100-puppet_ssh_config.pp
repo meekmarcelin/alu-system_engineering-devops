a1249c1b666560e4fc436eb2f192807bda861638
meekmarcelin <meekmarcelin@gmail.com>
2022-09-18 10:04:18 -0700

    100 commit

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash
+# make changes to  configuration file
 exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
         path    => '/bin/'
+}
